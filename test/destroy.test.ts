a3031838e3a54f71d2d6d0247070fd426c82dd6e
Dallas Hoffman <dallashoff@gmail.com>
2023-07-15 15:41:10 -0400

    Add test for destroy method

---
+++ test/destroy.test.ts
@@ -0,0 +1,21 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { SQLocal } from '../src/index';
+
+describe('destroy', () => {
+	const { sql, destroy } = new SQLocal('destroy-test.sqlite3');
+
+	beforeEach(async () => {
+		await sql`CREATE TABLE groceries (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)`;
+	});
+
+	it('should destroy the client', async () => {
+		const insert1 = await sql`INSERT INTO groceries (name) VALUES ('pasta') RETURNING name`;
+		expect(insert1).toEqual([{ name: 'pasta' }]);
+
+		await destroy();
+
+		const insert2Fn = async () =>
+			await sql`INSERT INTO groceries (name) VALUES ('sauce') RETURNING name`;
+		await expect(insert2Fn).rejects.toThrowError();
+	});
+});
