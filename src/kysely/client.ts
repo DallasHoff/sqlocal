036fcffb0026527a549bf008a986622a5ef471ef
Dallas Hoffman <dallashoff@gmail.com>
2023-07-15 00:46:34 -0400

    Create Kysely dialect

---
+++ src/kysely/client.ts
@@ -0,0 +1,62 @@
+import { SQLocal } from '..';
+import {
+	CompiledQuery,
+	DatabaseConnection,
+	Dialect,
+	Driver,
+	QueryResult,
+	SqliteAdapter,
+	SqliteIntrospector,
+	SqliteQueryCompiler,
+} from 'kysely';
+
+export class SQLocalKysely extends SQLocal {
+	private executor = async <T>(query: CompiledQuery) => {
+		const { rows, columns } = await this.exec(query.sql, query.parameters as any[], 'all');
+		return {
+			rows: this.convertRowsToObjects(rows, columns) as T[],
+		} satisfies QueryResult<T>;
+	};
+
+	dialect = {
+		createAdapter: () => new SqliteAdapter(),
+		createDriver: () => new SQLocalKyselyDriver(this.executor),
+		createIntrospector: (db) => new SqliteIntrospector(db),
+		createQueryCompiler: () => new SqliteQueryCompiler(),
+	} satisfies Dialect;
+}
+
+class SQLocalKyselyDriver implements Driver {
+	private executor: SQLocalKysely['executor'];
+
+	constructor(executor: SQLocalKysely['executor']) {
+		this.executor = executor;
+	}
+
+	async acquireConnection() {
+		return new SQLocalKyselyConnection(this.executor);
+	}
+
+	async init() {}
+	async beginTransaction() {}
+	async commitTransaction() {}
+	async rollbackTransaction() {}
+	async releaseConnection() {}
+	async destroy() {}
+}
+
+class SQLocalKyselyConnection implements DatabaseConnection {
+	private executor: SQLocalKysely['executor'];
+
+	constructor(executor: SQLocalKysely['executor']) {
+		this.executor = executor;
+	}
+
+	async executeQuery<T>(query: CompiledQuery) {
+		return await this.executor<T>(query);
+	}
+
+	async *streamQuery() {
+		throw new Error('SQLite3 does not support streaming.');
+	}
+}
