7988441c8b2518dd796b95005e5d618f38630d71
Askar Yusupov <devex.soft@gmail.com>
2023-11-04 17:21:53 +0500

    feat: add migrate function

--- src/drizzle/migrations.ts
+++ src/drizzle/migrations.ts
@@ -1,9 +1,11 @@
-// original file https://github.com/drizzle-team/drizzle-orm/blob/main/drizzle-orm/src/migrator.ts
-import {
+// ref https://github.com/drizzle-team/drizzle-orm/blob/main/drizzle-orm/src/migrator.ts
+import { sql } from 'drizzle-orm';
+import type {
 	KitConfig,
 	MigrationConfig,
 	MigrationMeta,
 } from 'drizzle-orm/migrator';
+import type { SqliteRemoteDatabase } from 'drizzle-orm/sqlite-proxy';
 
 async function readFile(name: string): Promise<string> {
 	const path = `${name}?raw`;
@@ -77,4 +79,46 @@ export async function readMigrationFiles(
 	}
 
 	return migrationQueries;
+}
+
+// ref https://github.com/drizzle-team/drizzle-orm/blob/main/drizzle-orm/src/sqlite-proxy/migrator.ts
+export type ProxyMigrator = (migrationQueries: string[]) => Promise<void>;
+
+export async function migrate<TSchema extends Record<string, unknown>>(
+	db: SqliteRemoteDatabase<TSchema>,
+	callback: ProxyMigrator,
+	config: string | MigrationConfig
+) {
+	const migrations = await readMigrationFiles(config);
+
+	const migrationTableCreate = sql`
+		CREATE TABLE IF NOT EXISTS "__drizzle_migrations" (
+			id SERIAL PRIMARY KEY,
+			hash text NOT NULL,
+			created_at numeric
+		)
+	`;
+
+	await db.run(migrationTableCreate);
+
+	const dbMigrations = await db.values<[number, string, string]>(
+		sql`SELECT id, hash, created_at FROM "__drizzle_migrations" ORDER BY created_at DESC LIMIT 1`
+	);
+
+	const lastDbMigration = dbMigrations[0] ?? undefined;
+
+	const queriesToRun: string[] = [];
+	for (const migration of migrations) {
+		if (
+			!lastDbMigration ||
+			Number(lastDbMigration[2])! < migration.folderMillis
+		) {
+			queriesToRun.push(
+				...migration.sql,
+				`INSERT INTO "__drizzle_migrations" ("hash", "created_at") VALUES('${migration.hash}', '${migration.folderMillis}')`
+			);
+		}
+	}
+
+	await callback(queriesToRun);
 }
--- test/drizzle/migrations.test.ts
+++ test/drizzle/migrations.test.ts
@@ -1,12 +1,35 @@
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
-import { readMigrationFiles } from '../../src/drizzle/migrations';
-import { MigrationConfig } from 'drizzle-orm/migrator';
 
-describe('readMigrationFiles', () => {
-	it('should read files', async () => {
-		const config: MigrationConfig = {
-			migrationsFolder: '../../test/drizzle/migrations/drizzle',
-		};
+import { readMigrationFiles, migrate } from '../../src/drizzle/migrations';
+import { SQLocalDrizzle } from '../../src/drizzle';
+
+//import { sql } from 'drizzle-orm';
+import { MigrationConfig } from 'drizzle-orm/migrator';
+import { drizzle } from 'drizzle-orm/sqlite-proxy';
+
+describe('migrations', () => {
+	const { driver, sql } = new SQLocalDrizzle('drizzle-driver-test.sqlite3');
+	const db = drizzle(driver);
+	const config: MigrationConfig = {
+		migrationsFolder: '../../test/drizzle/migrations/drizzle',
+	};
+
+	it('readMigrationFiles should read files', async () => {
 		const result = await readMigrationFiles(config);
+	});
+
+	it('should migrate', async () => {
+		await migrate(
+			db,
+			async (queries) => {
+				console.log(queries);
+			},
+			config
+		);
+
+		const result =
+			await sql`SELECT name FROM sqlite_schema WHERE type='table' AND name='__drizzle_migrations'`;
+
+		console.log(result);
 	});
 });
