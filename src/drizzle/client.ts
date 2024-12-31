0a46b45031a3a0ea2c125e14eddb0e8a8f969cf9
Dallas Hoffman <dallashoff@gmail.com>
2024-12-31 15:48:52 -0500

    Send Drizzle transaction warning only once per session

--- src/drizzle/client.ts
+++ src/drizzle/client.ts
@@ -7,10 +7,14 @@ export class SQLocalDrizzle extends SQLocal {
 		params: unknown[],
 		method: Sqlite3Method
 	): Promise<RawResultData> => {
-		if (/^begin\b/i.test(sql)) {
+		if (
+			/^begin\b/i.test(sql) &&
+			!sessionStorage._sqlocal_sent_drizzle_transaction_warning
+		) {
 			console.warn(
 				"Drizzle's transaction method cannot isolate transactions from outside queries. It is recommended to use the transaction method of SQLocalDrizzle instead (See https://sqlocal.dallashoffman.com/api/transaction#drizzle)."
 			);
+			sessionStorage._sqlocal_sent_drizzle_transaction_warning = '1';
 		}
 		return this.exec(sql, params, method);
 	};
