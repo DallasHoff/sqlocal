3b358199c6ace2a0e8530e44753b5fc954e04b52
Dallas Hoffman <dallashoff@gmail.com>
2024-12-31 16:37:34 -0500

    Try out alternate Vite header config

--- vite.config.ts
+++ vite.config.ts
@@ -18,17 +18,10 @@ export default defineConfig({
 	optimizeDeps: {
 		exclude: ['@sqlite.org/sqlite-wasm'],
 	},
-	plugins: [
-		{
-			enforce: 'pre',
-			name: 'configure-response-headers',
-			configureServer: (server) => {
-				server.middlewares.use((_req, res, next) => {
-					res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-					res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-					next();
-				});
-			},
+	server: {
+		headers: {
+			'Cross-Origin-Embedder-Policy': 'require-corp',
+			'Cross-Origin-Opener-Policy': 'same-origin',
 		},
-	],
+	},
 });
