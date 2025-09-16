35f8b8a3947f528c6426ec930f38ecd191f09e2b
Dallas Hoffman <dallashoff@gmail.com>
2025-09-15 23:27:51 -0400

    Test unrelated table update

--- test/reactive-query.test.ts
+++ test/reactive-query.test.ts
@@ -8,6 +8,7 @@ import {
 	vi,
 } from 'vitest';
 import { SQLocal } from '../src/client.js';
+import { sleep } from './test-utils/sleep.js';
 
 describe.each([
 	{ type: 'opfs', path: 'reactive-query-test.sqlite3' },
@@ -19,10 +20,12 @@ describe.each([
 
 	beforeEach(async () => {
 		await db1.sql`CREATE TABLE groceries (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)`;
+		await db1.sql`CREATE TABLE todos (title TEXT NOT NULL)`;
 	});
 
 	afterEach(async () => {
 		await db1.sql`DROP TABLE groceries`;
+		await db1.sql`DROP TABLE todos`;
 	});
 
 	afterAll(async () => {
@@ -36,23 +39,32 @@ describe.each([
 		async () => {
 			let list1: string[] = [];
 			let list2: string[] = [];
+			let todosUpdated = false;
+
 			const reactive1 = db1.reactiveQuery(
 				(sql) => sql`SELECT * FROM groceries`
 			);
 			const reactive2 = db2.reactiveQuery(
 				(sql) => sql`SELECT * FROM groceries`
 			);
-			const { unsubscribe: unsubscribe1 } = reactive1.subscribe(
-				(data) => (list1 = data.map(({ name }) => name))
+			const reactiveExtra = db2.reactiveQuery(
+				(sql) => sql`SELECT * FROM todos`
 			);
-			const { unsubscribe: unsubscribe2 } = reactive2.subscribe(
-				(data) => (list2 = data.map(({ name }) => name))
+			const callback1 = vi.fn(
+				(data: Record<string, any>[]) => (list1 = data.map(({ name }) => name))
 			);
+			const callback2 = vi.fn(
+				(data: Record<string, any>[]) => (list2 = data.map(({ name }) => name))
+			);
+			const { unsubscribe: unsubscribe1 } = reactive1.subscribe(callback1);
+			const { unsubscribe: unsubscribe2 } = reactive2.subscribe(callback2);
+			reactiveExtra.subscribe(() => (todosUpdated = true));
 
 			let expected: string[] = [];
 			expect(list1).toEqual(expected);
 			expect(list2).toEqual(expected);
 
+			// Insert
 			await db1.sql`INSERT INTO groceries (name) VALUES ('bread'), ('eggs')`;
 			await vi.waitUntil(() => list1.length === 2 && list2.length === 2);
 
@@ -60,6 +72,7 @@ describe.each([
 			expect(list1).toEqual(expected);
 			expect(list2).toEqual(expected);
 
+			// Update
 			await db2.sql`UPDATE groceries SET name = 'wheat bread' WHERE name = 'bread'`;
 			await vi.waitUntil(
 				() => list1.includes('wheat bread') && list2.includes('wheat bread')
@@ -69,6 +82,7 @@ describe.each([
 			expect(list1).toEqual(expected);
 			expect(list2).toEqual(expected);
 
+			// Delete
 			await db1.sql`DELETE FROM groceries WHERE name = 'wheat bread'`;
 			await vi.waitUntil(() => list1.length === 1 && list2.length === 1);
 
@@ -76,9 +90,21 @@ describe.each([
 			expect(list1).toEqual(expected);
 			expect(list2).toEqual(expected);
 
+			// Edit unrelated table
+			todosUpdated = false;
+			const prevCallCount = callback1.mock.calls.length;
+			await db1.sql`INSERT INTO todos (title) VALUES ('laundry')`;
+			await vi.waitUntil(() => todosUpdated === true);
+			await sleep(100);
+
+			const newCallCount = callback1.mock.calls.length;
+			expect(newCallCount).toBe(prevCallCount);
+
+			// Unsubscribe
 			unsubscribe1();
 			await db1.sql`INSERT INTO groceries (name) VALUES ('rice')`;
 			await vi.waitUntil(() => list2.length === 2);
+			await sleep(100);
 
 			expect(list1).toEqual(['eggs']);
 			expect(list2).toEqual(['eggs', 'rice']);
