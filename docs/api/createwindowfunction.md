393b101f2ed91601a6f8c88cd26bf751b841d986
Dallas Hoffman <dallashoff@gmail.com>
2026-04-21 00:24:13 -0400

    Add the standard test and doc boilerplate

--- docs/api/createwindowfunction.md
+++ docs/api/createwindowfunction.md
@@ -1,3 +1,19 @@
 # createWindowFunction
 
 Create a SQL function that can be called from queries to perform calculations for rows using data from related rows.
+
+## Usage
+
+Access or destructure `createWindowFunction` from the `SQLocal` client.
+
+```javascript
+import { SQLocal } from 'sqlocal';
+
+const { createWindowFunction } = new SQLocal('database.sqlite3');
+```
+
+<!-- @include: ../.partials/initialization-note.md -->
+
+TODO
+
+<!-- @include: ../.partials/functions-note.md -->
--- test/create-window-function.test.ts
+++ test/create-window-function.test.ts
@@ -1,1 +1,29 @@
-// TODO
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from 'vitest';
+import { SQLocal } from '../src/index.js';
+import { testVariation } from './test-utils/test-variation.js';
+
+describe.each(testVariation('create-window-function'))(
+	'createWindowFunction ($type)',
+	({ path }) => {
+		const { sql, createWindowFunction } = new SQLocal(path);
+
+		beforeAll(async () => {
+			const values = new Map<unknown, number>();
+
+			await createWindowFunction('', {
+				step: () => {},
+				value: () => {},
+				inverse: () => {},
+				final: () => {},
+			});
+		});
+
+		beforeEach(async () => {
+			await sql`CREATE TABLE nums (num REAL NOT NULL)`;
+		});
+
+		afterEach(async () => {
+			await sql`DROP TABLE nums`;
+		});
+	}
+);
